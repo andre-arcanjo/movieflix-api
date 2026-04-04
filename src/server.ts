0f1237c3af5e648cebc137ac2fc42ab1b7fa2244
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-04-04 14:21:36 -0300

    add movie sorting, genre and language

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,16 @@ const app = express();
 
 const prisma = new PrismaClient();
 
-app.get('/movies', async (req, res) => {
-    const movies = await prisma.movies.findMany();
+app.get('/movies', async (_, res) => {
+    const movies = await prisma.movies.findMany({
+        orderBy: { 
+            title:'asc'
+        },
+        include: {
+            genres: true,
+            languages: true,
+        }
+    });
     res.json(movies)
 });
 
