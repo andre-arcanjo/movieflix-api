1601ad7a8a2e83056ad194c4f06992bc3d4502a0
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-04-04 15:48:38 -0300

    add an error message during movie creation

--- src/server.ts
+++ src/server.ts
@@ -7,17 +7,48 @@ const app = express();
 
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movies.findMany({
-        orderBy: { 
-            title:'asc'
+        orderBy: {
+            title: 'asc',
         },
         include: {
             genres: true,
             languages: true,
-        }
+        },
     });
-    res.json(movies)
+    res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+
+    try {
+        const movieWithSameTitle = await prisma.movies.findFirst({
+            where: { title: { equals: title, mode: "insensitive" } },
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409).send('Já existe um filme cadastrado com esse título');
+        }
+
+        await prisma.movies.create({
+            data: {
+                title: title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+    } catch (error) {
+        return res.status(500).send({ message: 'Falha ao cadastrar filme' });
+    }
+
+    res.status(201).send();
 });
 
 app.listen(port, () => {
