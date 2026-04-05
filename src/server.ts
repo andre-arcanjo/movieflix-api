7c2b049e1423444ca4fab1701048437d4c1e8f8d
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-04-05 17:04:14 -0300

    feat: add movie filter for genre

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { PrismaClient } from './generated/prisma/index.js';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const port = 3000;
 
@@ -8,6 +10,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movies.findMany({
@@ -106,13 +109,39 @@ app.delete('/movies:id', async (req, res) => {
                 id,
             }
         })
-    }catch(error){
-        return res.status(500).send({message: 'Não foi possível remover o filme'})
+    } catch (error) {
+        return res.status(500).send({ message: 'Não foi possível remover o filme' })
     }
 
     res.status(200).send();
 });
 
+app.get('/movies/:genreName', async (req, res) => {
+
+    try {
+        const moviesFilteredGenreName = await prisma.movies.findMany({
+            include: {
+                genres: true,
+                languages: true
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: req.params.genreName,
+                        mode: 'insensitive'
+                    }
+                }
+            }
+        })
+        res.status(200).send(moviesFilteredGenreName)
+    } catch (error) {
+        return res.status(500).send({ message: 'Falha ao filtrar filmes por gênero' })
+    }
+
+})
+
 app.listen(port, () => {
     console.log(`Servidor em execução em: http://localhost:${port}`);
 });
+
+// adicionado filtro de filmes por genero
